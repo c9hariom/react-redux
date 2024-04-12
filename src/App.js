7cff93de1a963d582c1d3aab64514bf04856bae8
Hariom Singh <22169420+c9hariom@users.noreply.github.com>
2024-04-12 14:29:36 +0530

    designed front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <h1>Increment/Decrement Counter</h1>
+        <div className='quantity'>
+          <a className='quantity_minus'>
+            <span>-</span>
+          </a>
+          <input
+            type='text'
+            name='quantity'
+            className='quantity_input'
+            value='0'
+          />
+          <a className='quantity_plus'>
+            <span>+</span>
+          </a>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
